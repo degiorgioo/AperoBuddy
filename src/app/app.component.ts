cc33e9cfb19e1bf235531c4c6f27a0f7beebbdc0
Davide De Giorgio <davide.degiorgio@namics.com>
2017-10-12 16:21:23 +0200

    Some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LoggerService} from './logger.service';
+import {CrawlerService} from './crawler.service';
+
+import * as Crawler from 'js-crawler';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +12,14 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'AlphabetDrinks';
+
+  constructor(private logger: LoggerService, private crawler: CrawlerService) {
+  }
+
+  click() {
+    new Crawler().crawl('http://www.google.ch', function onSuccess(page) {
+      console.log(page.url);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LoggerService} from './logger.service';
+import {CrawlerService} from './crawler.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LoggerService, CrawlerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
